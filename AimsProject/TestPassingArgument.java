01c0433efa573d7c36b734cc7ff95b22395b845f
Doccocaubai710 <hoangminhhoang07102003@gmail.com>
2023-05-28 11:05:13 +0700

    class members

--- AimsProject/TestPassingArgument.java
+++ AimsProject/TestPassingArgument.java
@@ -9,13 +9,13 @@ public class TestPassingArgument {
         changeTitle(jungleDVD,cinderellaDVD.getTitle());
         System.out.println("jungle dvd title: " + jungleDVD.getTitle());
     }
-    // public static void swap(Object o1, Object o2){
-    //     Object tmp = o1;
-    //     o1 = o2;
-    //     o2 = tmp;
-    // }
+    public static void swap(Object o1, Object o2){
+        Object tmp = o1;
+        o1 = o2;
+        o2 = tmp;
+    }
 
-    //overloading
+    
     public static void swap(DigitalVideoDisc dvd1, DigitalVideoDisc dvd2) {
 	    DigitalVideoDisc tmp = new DigitalVideoDisc(dvd1.getTitle(), dvd1.getCategory(), dvd1.getDirector(), dvd1.getLength(), dvd1.getCost());
 	    dvd1.setTitle(dvd2.getTitle());
